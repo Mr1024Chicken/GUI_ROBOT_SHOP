cfb0c58599272e54c972a84df7a2da956d3cb1e3
kingko555 <kha.vo@mavs.uta.edu>
2016-11-16 19:57:35 -0600

    menu bar and dialog box added

--- Robot_shop.cpp
+++ Robot_shop.cpp
@@ -11,64 +11,63 @@
 #include <FL/Fl_Choice.H>
 #include <FL/fl_ask.H>
 #include <FL/Fl_Button.H>
-
-#include "Viewer.h"
-
+#include <FL/Fl_Menu.H>
+#include <FL/Fl_Multiline_Input.H>
+#include <FL/Fl_Return_Button.H>
+#include <FL/Fl_Input.H>
 using namespace std;
 
-	Robo ob;
-	Controller contr;
-	Viewer hawk;
+#ifndef Input_Dialog_H
+#define Input_Dialog_H
+
+class Input_Dialog{
+public:
+
+	Input_Dialog(){
+	dialog = new Fl_Window(340,300,"Robot Parts");
+
+	name = new Fl_Input(120,40,210,25,"Name:");
+
+	serial = new Fl_Input(120,70,210,25,"Serial NUmber:");
 	
+	weight = new Fl_Input(120,100,210,25,"Weight:");
+	
+	cost = new Fl_Input(120,130,130,25,"Cost:");
 
-Fl_Window *win;
+	description = new Fl_Multiline_Input(120,160,210,75,"Description:");
+	
+	create = new Fl_Return_Button(145,240,120,25,"Create");
 
-void CLoseCB(Fl_Widget*  w, void* p){
-	Fl_Window *win = (Fl_Window *)w ;
-	if(fl_choice("Are You Sure?","Random",fl_no,fl_yes)==2){
-		win->hide();
+	cancel = new Fl_Button(270,240,60,25,"Cancel");
+
+	dialog->end();
+	dialog->set_non_modal();
 	}
-}
-
-void ButtonCB(Fl_Widget* w, void *p){
-
-Fl_Button *b= (Fl_Button*)w;
-ob.print_body(ob.get_body());
-}
-
-void Button2CB(Fl_Widget* w, void *p){
-
-Fl_Button *b= (Fl_Button*)w;
-ob = contr.Test_add(ob);
-}
-
-
-int main(){
-
-	//hawk.menu(ob,contr);
-
-	const int x = 800, y= 600;
-
-	Fl_Window *win = new Fl_Window(x,y,"$$-Robot Shop-$$");
-
-	Fl_Button *button= new Fl_Button(0,90,140,50,"view torso!");
-	Fl_Button *button2 = new Fl_Button(0,40,140,50,"add parts!");
-
-	win->callback(CLoseCB);
-
-	//Fl_Box *box = new Fl_Box(20,40,300,100,"NOOOOOOO!!!!!!");
-	button->callback(ButtonCB,"view torso!");
-
-
-	button2->callback(Button2CB,"add parts!");
+	void show(){dialog->show();}
+	void hide() {dialog->hide();}
+	string name(){return name->value();}
+	string serial(){return serial->value();}
+	string weight(){return weight->value();}
+	string cost(){return cost->value();}
+	string description(){return description->value();}
 	
+	/*string name(){return (*name).value();}
+	string serial(){return (*serial).value();}
+	string weight(){return (*weight).value();}
+	string cost(){return (*cost).value();}
+	string description(){return (*description).value();}
+	*/
+private:
 	
-	
-	cout <<endl<<"done"<<endl;
-	win->end();
-	win->show();
+	Fl_Window *dialog;
+	Fl_Input *name;
+	Fl_Input *serial;
+	Fl_Input *weight;
+	Fl_Input *cost;
+	Fl_Input *description;
+	Fl_Return_Button *create;
+	Fl_Button *cancel;
+};
 
-	return Fl::run();
-}
 
-
+#endif
